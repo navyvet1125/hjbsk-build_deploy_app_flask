a3be4d47c462593ca80e32b82e900dd2f1e9ba03
Evan Washington <enavy04@gmail.com>
2024-05-07 23:38:45 -0700

    Refactored code to only need one function.

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,23 +1,15 @@
-let runAddition = () => {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
+// Description: This script is used to make a request to the server to perform an operation
 
-    let result = num1 + num2;
-    document.getElementById("system_response").innerHTML = "Result: " + result;
-};
-
-let runSubtraction = () => {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-
-    let result = num1 - num2;
-    document.getElementById("system_response").innerHTML = "Result: " + result;
-};
-
-let runMultiplication = () => {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-
-    let result = num1 * num2;
-    document.getElementById("system_response").innerHTML = "Result: " + result;
+let runOperation = (operator) => {
+    if (operator === "sum" || operator === "sub" || operator === "mul" || operator === "div") {
+        let num1 = parseFloat(document.getElementById("num1").value);
+        let num2 = parseFloat(document.getElementById("num2").value);
+        const url = `http://localhost:5000/${operator}?num1=${num1}&num2=${num2}`;
+        fetch(url)
+        .then(response => response.json())
+        .then(res => document.getElementById("system_response").innerHTML = "Result: " + res.result)
+        .catch(err => document.getElementById("system_response").innerHTML = "Error: " + err);    
+    } else {
+        document.getElementById("system_response").innerHTML = "Invalid operator";
+    }
 };
