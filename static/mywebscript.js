7b7fc2ae91b340a6f4f1438720d33fb107785698
Evan Washington <enavy04@gmail.com>
2024-05-22 19:57:37 -0700

    implemented jquery functionality

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,38 +1,41 @@
 // Description: This script is used to make a request to the server to perform an operation
 
+const OPS_OPTIONS = ["sum", "sub", "mul", "div", "mod", "pow", "intdiv", "pythag"];
+
+
 const runOperation = async (op) => {
-    if (op === "sum" || op === "sub" || op === "mul" || op === "div" || op === "mod" || op === "pow" || op === "intdiv" || op ==="pythag") {
-        let num1 = parseFloat(document.getElementById("num1").value);
-        let num2 = parseFloat(document.getElementById("num2").value);
+    if (OPS_OPTIONS.includes(op)) {
+        let num1 = parseFloat($("#num1").val());
+        let num2 = parseFloat($("#num2").val());
         const url = `http://192.168.12.161:5000/${op}?num1=${num1}&num2=${num2}`;
         
         try {
             let res = await axios.get(url);
             let result = res.data.result;
-            document.getElementById("system_response").innerHTML += `<li>Result: ${result} </li>`;
+            $("#system_response").append(`<li>Result: ${result} </li>`);
         } catch (err) {
             let result = err.response.data.result;
-            document.getElementById("system_response").innerHTML += `<li class='alert alert-danger'>Error: ${result} </li>`;
+            $("#system_response").append(`<li class='alert alert-danger'>Error: ${result} </li>`);
         }
     } else {
-        document.getElementById("system_response").innerHTML += "<li class='alert alert-danger'> Invalid operation! </li>";
+        $("#system_response").append("<li class='alert alert-danger'> Invalid operation! </li>");
     }
-    document.getElementById("clearButton").style.display = "block";
+    $("#clearButton").show();
 };
 
 const sayHello = async () => {
     try {
         let res = await axios.get("http://192.168.12.161:5000/hello")
         let data = res.data
-        document.getElementById("system_response").innerHTML += `<li> ${data.message} </li>`;
+        $("#system_response").append(`<li> ${data.message} </li>`);
     } catch (err) {
-        document.getElementById("system_response").innerHTML += `<li>Error: ${err} </li>`;
+        $("#system_response").append(`<li>Error: ${err} </li>`);
     }
-    document.getElementById("clearButton").style.display = "block";
+    $("#clearButton").show();
 }
 
 // Clear system response
 const clearResponse = () => {
-    document.getElementById("system_response").innerHTML = "";
-    document.getElementById("clearButton").style.display = "none";
+    $("#system_response").html("");
+    $("#clearButton").hide();
 }
